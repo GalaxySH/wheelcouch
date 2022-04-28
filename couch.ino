7aca51f42318d655812d32ce2a1b71c7f72913ab
GalaxySH <stefan@hauge.rocks>
2022-04-27 22:51:04 -0500

    failed servo.h implement

--- couch.ino
+++ couch.ino
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <SoftPWM.h>
+#include <Servo.h>
 #include "PinDefinitionsAndMore.h"
 
 #define IRMP_PROTOCOL_NAMES              1 // Enable protocol number mapping to protocol strings - needs some FLASH. Must before #include <irmp*>
@@ -14,12 +14,10 @@
 
 #include <irmp.hpp>
 
-/*-----( Global Constants )-----*/
-int32_t frequency = 750;//frequency (in Hz)
-bool success;
+/*-----( VARS )-----*/
 int REVERSE = 0;
-int NEUTRAL = 128;//127// confirm that this is actually a neutral value
-int FORWARD = 255;
+int NEUTRAL = 90;//90// confirm that this is actually a neutral value
+int FORWARD = 180;
 int motors[2][2] {{9, 10}, {5, 6}};// left forward, left rear, right forward, right rear
 int speeds[2] {NEUTRAL, NEUTRAL};// left, right
 int fpow = 0;// initial forward power  (vector with values -1, 0, and 1)
@@ -27,8 +25,12 @@ int lpow = 0;// initial lateral turning power (vector with values -1, 0, and 1)
 String lastPressed = "";
 IRMP_DATA irmp_data;
 bool sJustReceived;
-
 void handleReceivedIRData();
+Servo lfDrive;
+Servo lrDrive;
+Servo rfDrive;
+Servo rrDrive;
+
 /*
    Helper macro for getting a macro definition as string
 */
@@ -37,46 +39,7 @@ void handleReceivedIRData();
 #define STR(x) STR_HELPER(x)
 #endif
 
-/* pins_arduino.h defines the pin-port/bit mapping as PROGMEM so
-   you have to read them with pgm_read_xxx(). That's generally okay
-   for ordinary use, but really bad when you're writing super fast
-   codes because the compiler doesn't treat them as constants and
-   cannot optimize them away with sbi/cbi instructions.
-
-   Therefore we have to tell the compiler the PORT and BIT here.
-   Hope someday we can find a way to workaround this.
-
-   Check the manual of your MCU for port/bit mapping.
-
-   The following example demonstrates setting channels for all pins
-   on the ATmega328P or ATmega168 used on Arduino Uno, Pro Mini,
-   Nano and other boards. */
-//SOFTPWM_DEFINE_CHANNEL(0, DDRD, PORTD, PORTD0);  //Arduino pin 0
-//SOFTPWM_DEFINE_CHANNEL(1, DDRD, PORTD, PORTD1);  //Arduino pin 1
-//SOFTPWM_DEFINE_CHANNEL(2, DDRD, PORTD, PORTD2);  //Arduino pin 2
-//SOFTPWM_DEFINE_CHANNEL(2, DDRD, PORTD, PORTD3);  //Arduino pin 3
-//SOFTPWM_DEFINE_CHANNEL(4, DDRD, PORTD, PORTD4);  //Arduino pin 4
-SOFTPWM_DEFINE_CHANNEL(0, DDRD, PORTD, PORTD5);  //Arduino pin 5
-SOFTPWM_DEFINE_CHANNEL(1, DDRD, PORTD, PORTD6);  //Arduino pin 6
-//SOFTPWM_DEFINE_CHANNEL(7, DDRD, PORTD, PORTD7);  //Arduino pin 7
-//SOFTPWM_DEFINE_CHANNEL(8, DDRB, PORTB, PORTB0);  //Arduino pin 8
-SOFTPWM_DEFINE_CHANNEL(2, DDRB, PORTB, PORTB1);  //Arduino pin 9
-SOFTPWM_DEFINE_CHANNEL(3, DDRB, PORTB, PORTB2);  //Arduino pin 10
-SOFTPWM_DEFINE_CHANNEL(4, DDRB, PORTB, PORTB3);  //Arduino pin 11
-//SOFTPWM_DEFINE_CHANNEL(12, DDRB, PORTB, PORTB4);  //Arduino pin 12
-//SOFTPWM_DEFINE_CHANNEL(13, DDRB, PORTB, PORTB5);  //Arduino pin 13
-//SOFTPWM_DEFINE_CHANNEL(14, DDRC, PORTC, PORTC0);  //Arduino pin A0
-//SOFTPWM_DEFINE_CHANNEL(15, DDRC, PORTC, PORTC1);  //Arduino pin A1
-//SOFTPWM_DEFINE_CHANNEL(16, DDRC, PORTC, PORTC2);  //Arduino pin A2
-//SOFTPWM_DEFINE_CHANNEL(17, DDRC, PORTC, PORTC3);  //Arduino pin A3
-//SOFTPWM_DEFINE_CHANNEL(18, DDRC, PORTC, PORTC4);  //Arduino pin A4
-//SOFTPWM_DEFINE_CHANNEL(19, DDRC, PORTC, PORTC5);  //Arduino pin A5
-
-/* Here you make an instance of desired channel counts you want
-   with the default 256 PWM levels (0 ~ 255). */
-SOFTPWM_DEFINE_OBJECT(5);
-
-/*-----( Function )-----*/
+/*-----( FUNCS )-----*/
 void handleReceivedIRData() {
     irmp_get_data(&irmp_data);
 #if defined(ARDUINO_ARCH_MBED) || defined(ESP32)
@@ -105,7 +68,7 @@ String translateIR() {
     case 0xD: return "ST/REPT";
     case 0x16: return "0";
     case 0xC: return "1";
-    case 018: return "2";
+    case 0x18: return "2";
     case 0x5E: return "3";
     case 0x8: return "4";
     case 0x1C: return "5";
@@ -132,32 +95,9 @@ void controlExec(String cmd) {
   } else if (cmd == "POWER") {
     allStop();
   } else if (cmd == "FUNC/STOP") {
-    channelTest();
+    return;//make functionality test function
   } else {
     return;
-  }
-}
-
-//LOOP THROUGH AND FADE IN CHANNELS
-static volatile uint8_t v = 0;
-void channelTest() {
-  for (uint8_t i = 0; i < Palatis::SoftPWM.size(); ++i) {
-    //Serial.print(micros());
-    //Serial.print(" loop(): ");
-    //Serial.println(i);
-
-    unsigned long const WAIT = 1000000 / Palatis::SoftPWM.PWMlevels() / 2;
-    unsigned long nextMicros = 0;
-    for (int v = 0; v < Palatis::SoftPWM.PWMlevels() - 1; ++v) {
-      while (micros() < nextMicros);
-      nextMicros = micros() + WAIT;
-      Palatis::SoftPWM.set(i, v);
-    }
-    for (int v = Palatis::SoftPWM.PWMlevels() - 1; v >= 0; --v) {
-      while (micros() < nextMicros);
-      nextMicros = micros() + WAIT;
-      Palatis::SoftPWM.set(i, v);
-    }
   }
 }
 
@@ -191,9 +131,11 @@ void setup() {
   // Just to know which program is running on my Arduino
 //  Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
 
-  //SOFTPWM
-  //Palatis::SoftPWM.begin(500);
-  Palatis::SoftPWM.printInterruptLoad();
+  //SERVO
+  lfDrive.attach(motors[0][0]);
+  lrDrive.attach(motors[0][1]);
+  rfDrive.attach(motors[1][0]);
+  rrDrive.attach(motors[1][1]);
 
   //IRRECEIVER
   pinMode(LED_BUILTIN, OUTPUT);
@@ -223,11 +165,11 @@ void loop() {
     case 1: right(); break;
     default: break;
   }
-  Palatis::SoftPWM.set(0, speeds[0]);
-  Palatis::SoftPWM.set(1, speeds[0]);
-  Palatis::SoftPWM.set(2, speeds[1]);
-  Palatis::SoftPWM.set(3, speeds[1]);
-  Palatis::SoftPWM.set(4, speeds[1]);
-  //  Serial.println(String("left: ") + String(speeds[0]) + String(" right: ") + String(speeds[1]));
 
+  lfDrive.write(speeds[0]);
+  lrDrive.write(speeds[0]);
+  rfDrive.write(speeds[1]);
+  rrDrive.write(speeds[1]);
+  
+  //  Serial.println(String("left: ") + String(speeds[0]) + String(" right: ") + String(speeds[1]));
 }
